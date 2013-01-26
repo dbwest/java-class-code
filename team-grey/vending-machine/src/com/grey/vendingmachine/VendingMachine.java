c2047f294922f58946d6b7cecc94a7099d76422f
Chris Vaughn <vaughncm@gmail.com>
2013-01-25 20:01:42 -0500

    Committing new tests and code

--- team-grey/vending-machine/src/com/grey/vendingmachine/VendingMachine.java
+++ team-grey/vending-machine/src/com/grey/vendingmachine/VendingMachine.java
@@ -1,7 +1,24 @@
 package com.grey.vendingmachine;
 
 public class VendingMachine {
-	public String getDisplayValue() {
-		return "INSERT COIN";
+	private String displayValue = "INSERT COIN";
+	private double cashAmt = 0;
+	
+	public String displayValue() {		
+		return displayValue;
+	}
+
+	public void insertCoin(String coin) {
+		if (coin == "Quarter") {
+			cashAmt = cashAmt + .25;
+			displayValue = String.format("%4.2f", cashAmt);
+		}
+		else if (coin == "Nickel") {
+			cashAmt = cashAmt + .05;
+			displayValue = String.format("%4.2f", cashAmt);
+		}
+		else {
+			displayValue = "INSERT COIN";
+		}	
 	}
 }
