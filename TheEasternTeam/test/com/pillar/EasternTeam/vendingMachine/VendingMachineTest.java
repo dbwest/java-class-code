53d61760723057b940e6b3e4aa8523dff639fec3
Sherief Gharraph <gharraph@msn.com>
2013-01-25 20:45:18 -0500

    Vending machine nickle,dime

--- TheEasternTeam/test/com/pillar/EasternTeam/vendingMachine/VendingMachineTest.java
+++ TheEasternTeam/test/com/pillar/EasternTeam/vendingMachine/VendingMachineTest.java
@@ -11,5 +11,18 @@ public class VendingMachineTest {
 		VendingMachine machine = new VendingMachine();
 		assertEquals("INSERT COIN",machine.getDisplay());
 	}
-
+	
+	@Test
+	public void itDesplays5CentsWhenNickleIsInserted() {
+		VendingMachine machine = new VendingMachine();
+		machine.insertCoin("nickle");
+		assertEquals("5 Cents", machine.getDisplay());
+	}
+	
+	@Test
+	public void itDesplays10CentsWhenDimeIsInserted() {
+		VendingMachine machine = new VendingMachine();
+		machine.insertCoin("dime");
+		assertEquals("10 Cents", machine.getDisplay());
+	}
 }
