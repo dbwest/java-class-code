8da81e1b98f40f215618ca186454ef111d94b909
Jonathan Moody <jonmoody12@gmail.com>
2013-01-25 19:38:38 -0500

    Tests written to accept coins

--- TheVirtualMountain/src/com/thevirtualmountain/vendingmachine/VendingMachine.java
+++ TheVirtualMountain/src/com/thevirtualmountain/vendingmachine/VendingMachine.java
@@ -6,4 +6,20 @@ public class VendingMachine {
 		return "INSERT COIN";
 	}
 
+	public String insertCoin(String coin) {
+		
+		if (coin.equals("Nickel")){
+			return "0.05";
+		}
+		else if (coin.equals("Dime")){
+			return "0.10";
+		}
+		else if (coin.equals("Quarter")) {
+			return "0.25";
+		}
+		else{
+			return "INSERT COIN";
+		}
+	}
+
 }
--- TheVirtualMountain/test/com/thevirtualmountain/vendingmachine/VendingMachineTest.java
+++ TheVirtualMountain/test/com/thevirtualmountain/vendingmachine/VendingMachineTest.java
@@ -2,15 +2,41 @@ package com.thevirtualmountain.vendingmachine;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class VendingMachineTest {
 
+	private VendingMachine machine;
+	
+	@Before
+	public void setup() {
+		machine = new VendingMachine();
+	}
+	
 	@Test
 	public void itDisplaysInsertCoinWhenNoCoinsInserted() {
-		VendingMachine machine = new VendingMachine();
-		
 		assertEquals("INSERT COIN", machine.getDisplay());
+	}
+	
+	@Test
+	public void itDisplaysFiveCentsWhenPassedNickel(){
+		assertEquals("0.05", machine.insertCoin("Nickel"));
+	}
+	
+	@Test
+	public void itDisplaysTenCentsWhenPassedDime() {
+		assertEquals("0.10", machine.insertCoin("Dime"));
+	}
+	
+	@Test
+	public void itDisplaysTenCentsWhenPassedQuarter() {
+		assertEquals("0.25", machine.insertCoin("Quarter"));
+	}
+	
+	@Test
+	public void itDisplaysInsertCoinWhenInvalidCoinInserted() {
+		assertEquals("INSERT COIN", machine.insertCoin("Penny"));
 	}
 
 }
