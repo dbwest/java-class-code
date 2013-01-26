a1b315ccde5896b0f11fb8cfaa0d3ee9f8d251b9
Chris Vaughn <vaughncm@gmail.com>
2013-01-25 20:50:00 -0500

    DRYed out

--- team-grey/vending-machine/src/com/grey/vendingmachine/VendingMachine.java
+++ team-grey/vending-machine/src/com/grey/vendingmachine/VendingMachine.java
@@ -5,23 +5,25 @@ public class VendingMachine {
 	private String displayValue = "INSERT COIN";
 	private double cashAmt = 0;
 	private ArrayList<String> returnSlot = new ArrayList<String>();
+	private Map<String, Double> coinTypes = new HashMap<String, Double>();
+	
+	public VendingMachine(){
+		coinTypes.put("Quarter", 0.25);
+		coinTypes.put("Dime", 0.10);
+		coinTypes.put("Nickel", 0.05);
+	}
 	
 	public String displayValue() {		
 		return displayValue;
 	}
 
 	public void insertCoin(String coin) {
-		if (coin == "Quarter") {
-			cashAmt = cashAmt + .25;
-			displayValue = String.format("%4.2f", cashAmt);
-		}
-		else if (coin == "Nickel") {
-			cashAmt = cashAmt + .05;
+		if (coinTypes.containsKey(coin)){
+			cashAmt += coinTypes.get(coin);
 			displayValue = String.format("%4.2f", cashAmt);
 		}
 		else {
 			returnSlot.add(coin);
-			displayValue = "INSERT COIN";
 		}	
 	}
 
