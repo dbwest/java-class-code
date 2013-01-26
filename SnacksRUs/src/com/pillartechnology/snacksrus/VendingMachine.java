2f493f1fd6c5aefb1a385bdb088d6d6616e1493b
James Sanders <arracos@gmail.com>
2013-01-25 21:08:23 -0500

    End of day

--- SnacksRUs/src/com/pillartechnology/snacksrus/VendingMachine.java
+++ SnacksRUs/src/com/pillartechnology/snacksrus/VendingMachine.java
@@ -2,9 +2,26 @@ package com.pillartechnology.snacksrus;
 
 public class VendingMachine {
 
+	public String coin;
+ 
 	public String getDisplay() {
-		
+
 		return "INSERT COIN";
 	}
+
+	public String acceptCoin(String coin) {
+		if (coin == "quarter") {
+			this.coin = "quarter";
+		} else if (coin == "dime") {
+			this.coin = "dime";
+		} else if (coin == "nickel") {
+			this.coin = "nickel";
+		} else {
+			return "coin returned: " + coin;
+		}
+
+		return this.coin;
+	}
+
 
 }
--- SnacksRUs/test/com/pillartechnology/snacksrus/VendingMachineTest.java
+++ SnacksRUs/test/com/pillartechnology/snacksrus/VendingMachineTest.java
@@ -1,15 +1,43 @@
 package com.pillartechnology.snacksrus;
 
 import static org.junit.Assert.assertEquals;
+
 import org.junit.Before;
 import org.junit.Test;
 
 public class VendingMachineTest {
 
+	private VendingMachine machine;
+
+	@Before
+	public void setup(){
+		machine = new VendingMachine();
+	}
+	
 	@Test
 	public void itDisplaysInsertCoinWhenCoinless(){
-		VendingMachine machine = new VendingMachine();
-		
 		assertEquals("INSERT COIN", machine.getDisplay());
 	}
+
+	
+	@Test
+	public void itAcceptsQuarter(){
+		assertEquals("quarter", machine.acceptCoin("quarter"));
+	}
+	
+	@Test
+	public void itAcceptsDime(){
+		assertEquals("dime", machine.acceptCoin("dime"));
+	}
+	
+	@Test
+	public void itAcceptsNickels(){
+		assertEquals("nickel", machine.acceptCoin("nickel"));
+	}
+	
+	@Test
+	public void itIsNotAValidCoin(){
+		assertEquals("coin returned: penny", machine.acceptCoin("penny"));
+	}
+	
 }
